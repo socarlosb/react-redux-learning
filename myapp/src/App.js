5101d697160589be700e8411a8d8029b768ab2a6
Carlos Barbosa <socarlosb@gmail.com>
2018-08-23 12:05:38 +0100

    create lists

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Digitals from './Components/Digitals';
 class App extends Component {
+  state = {
+    digitals: [
+      { name: 'Carlos', age: '36', city: 'Porto', id: 1 },
+      { name: 'Isa', age: '35', city: 'Lisbon', id: 2 },
+      { name: 'Bruno', age: '30', city: 'Porto', id: 3 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <Digitals name="Isa" age="35" city="Lisbon" />
-        <Digitals name="Carlos" age="36" city="Porto" />
+        <Digitals digitals={this.state.digitals} />
       </div>
     );
   }
--- myapp/src/Components/Digitals.js
+++ myapp/src/Components/Digitals.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react';
 class Digitals extends Component {
   render() {
     // console.log(this.props);
-    const { name, age, city } = this.props;
-
-    return (
-      <div className="digital">
-        <div>Name: {name}</div>
-        <div>Age: {age}</div>
-        <div>City: {city}</div>
-      </div>
-    );
+    const { digitals } = this.props;
+    const digitalList = digitals.map(digital => {
+      return (
+        <div className="digital" key={digital.id}>
+          <div>Name: {digital.name}</div>
+          <div>Age: {digital.age}</div>
+          <div>City: {digital.city}</div>
+        </div>
+      );
+    });
+    return <div className="digital-list">{digitalList}</div>;
   }
 }
 
