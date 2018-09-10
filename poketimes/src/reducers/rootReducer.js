fa77d965aa59d903db81524f5e96b414eb493bdf
Carlos Barbosa <socarlosb@gmail.com>
2018-09-10 16:05:30 +0100

    dispatch aka update our store

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -2,10 +2,24 @@ const initState = {
 	posts: [
 		{ id: '1', title: ' Funny story', body: 'lorem ipsum bla bla' },
 		{ id: '2', title: ' Funny story 2', body: 'lorem ipsum bla bla again' },
-		{ id: '3', title: ' Funny story 3', body: 'lorem ipsum bla bla one more time' },
-	],
+		{
+			id: '3',
+			title: ' Funny story 3',
+			body: 'lorem ipsum bla bla one more time'
+		}
+	]
 };
 const rootReducer = (state = initState, action) => {
+	if (action.type === 'DELETE_POST') {
+		let newPosts = state.posts.filter((post) => {
+			return action.id !== post.id;
+		});
+		return {
+			...state,
+			posts: newPosts
+		};
+	}
+
 	return state;
 };
 
